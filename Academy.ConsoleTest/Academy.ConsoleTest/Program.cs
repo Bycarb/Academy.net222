94b33413a1fc43406412abf332bc4cad0de13df7
Bycarb <apulisci@yahoo.it>
2022-04-19 18:02:43 +0200

    Fine lezione primo giorno.

--- Academy.ConsoleTest/Academy.ConsoleTest/Program.cs
+++ Academy.ConsoleTest/Academy.ConsoleTest/Program.cs
@@ -10,7 +10,21 @@ namespace Academy.ConsoleTest
     {
         static void Main(string[] args)
         {
+            string myString3 = null;
+            string myString2 = "test ";
+            myString3 = "stringa 3";
 
+            var result = myString2 + myString3;
+            result = string.Concat(myString2, myString3);
+
+            bool hasC = myString3.Contains("c");
+            int indexA = myString3.IndexOf("a");
+
+            Console.WriteLine(result);
+
+            myString3 = "";
+
+            Console.ReadKey();
         }
     }
 }
